64ecda4d41a0cb26feaf698d495cd7795f453e97
Liudmyla BEREZYNETS <cqmila@gmail.com>
2019-02-09 14:07:18 +0200

    game_logic folders

---
+++ inc/game_logic/Serialization.h
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serialization.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lberezyn <lberezyn@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/09 14:01:17 by lberezyn          #+#    #+#             */
+/*   Updated: 2019/02/09 14:01:17 by lberezyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BOMBERMAN_SERIALIZATION_H
+#define BOMBERMAN_SERIALIZATION_H
+
+#endif //BOMBERMAN_SERIALIZATION_H
