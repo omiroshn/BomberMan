a0d6138ece85ceeb7987f21e1e57eb1ef57d05b0
arxcel <vadim.k.394010@gmail.com>
2019-02-14 00:28:04 +0200

    [Graphics] Created basic camera

---
+++ inc/renderer/Camera.hpp
@@ -0,0 +1,56 @@
+//
+// Created by Vadim on 2019-02-13.
+//
+
+#ifndef BOMBERMAN_CAMERA_HPP
+#define BOMBERMAN_CAMERA_HPP
+
+#include "GL/glew.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+enum CameraDirection {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+// Default camera values
+const float SPEED       =  2.5f;
+const float SENSITIVITY =  0.1f;
+const float ZOOM        =  45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = 0.f, float pitch = 0.f);
+    glm::mat4 getViewMatrix();
+    void movaCamera(CameraDirection dir, float deltaTime);
+    void processMouseMovement(float xoffset, float yoffset);
+    void processMouseScroll(float zoomFactor);
+
+    float zoom() const;
+private:
+    void updateCameraVectors();
+
+private:
+    glm::vec3 mPosition;
+    glm::vec3 mFront;
+    glm::vec3 mUp;
+    glm::vec3 mRight;
+    glm::vec3 mWorldUp;
+
+    float mYaw;
+    float mPitch;
+
+    float mMovementSpeed;
+    float mMouseSensitivity;
+    float mZoom;
+};
+#endif //BOMBERMAN_CAMERA_HPP
--- inc/renderer/Renderer.hpp
+++ inc/renderer/Renderer.hpp
@@ -11,6 +11,7 @@
 #include "CustomException.hpp"
 #include "SpriteRenderer.hpp"
 #include "ResourceManager.hpp"
+#include "Camera.hpp"
 #include <map>
 
 class Renderer {
@@ -19,9 +20,13 @@ public:
     ~Renderer();
     std::string		processInput(bool &);
     void			preFrame();
+    void            frame();
     void			postFrame();
-    void			draw(std::string const &texture, glm::vec2 const &pos, glm::vec2 const &scale, float const &rot = 0.0f, glm::vec3 const &color = glm::vec3(1.0f));
-    void                            setProjection();
+    void			drawSprite(std::string const &texture, glm::vec2 const &pos, glm::vec2 const &scale, float const &rot = 0.0f, glm::vec3 const &color = glm::vec3(1.0f));
+    void            initSpriteRenderer();
+
+    Renderer(Renderer const &) = delete;
+    Renderer &operator=(Renderer const &) = delete;
 
 private:
     std::shared_ptr<SpriteRenderer>	mSpriteRenderer;
@@ -29,12 +34,36 @@ private:
     SDL_GLContext					mContext;
     SDL_Event						mE;
 private:
-    void			init(int width, int height, std::string const &winName);
+    void			init(std::string const &winName);
     void			deinit();
-    Renderer(Renderer const &);
-    Renderer &operator=(Renderer const &);
+    void            initSDL();
+    void            initGL();
+    void            createWindow(std::string const& winName);
+    void            mapInput();
+
+    //events
+private:
+    void processKeypressEvent(SDL_Event e);
+    void processMouseMoveEvent(SDL_Event e);
+    void processMouseWheelEvent(SDL_Event e);
     std::map<int, std::string> mIsBtnPressed;
     int mWidth, mHeight;
+    Camera cam;
+
+
+    //deltaTime
+    Uint64 mCurrentTime;
+    Uint64 mLastTime;
+    float mDeltaTime;
+
+    //test data
+    void initTestData();
+    unsigned int VBO, VAO;
+
+    float lastX;
+    float lastY;
+    bool firstMouse;
+
 };
 
 #endif //BOMBERMAN_Renderer_HPP
