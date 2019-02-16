b6a871ec5720ca3b84ddc545882684129d7bc3a7
arxcel <vadim.k.394010@gmail.com>
2019-02-17 00:00:52 +0200

    [GC] Created basic light system

--- Assets/shader/normalModel.ft.glsl
+++ Assets/shader/normalModel.ft.glsl
@@ -2,13 +2,29 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
+in vec3 Normal;
+in vec3 FragPos;
 
 // texture samplers
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+uniform vec3 lightPos;
+
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    vec4 lightColor = vec4(1.0,1.0,1.0,1.0);
+
+    //diffuse light
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec4 diffuse = diff * lightColor;
+
+    //ambient light
+    float ambientStrength = 0.1;
+    vec4 ambient = ambientStrength * lightColor;
+
+    vec4 objectColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = (ambient + diffuse) * objectColor;
 }
