0ec109d916cddad6ea29adeeb128098144efada8
Liudok <cqmila@gmail.com>
2019-02-18 05:56:29 +0200

    Instances created

--- inc/LogicCore/MapGeneration.hpp
+++ inc/LogicCore/MapGeneration.hpp
@@ -1,19 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MapGeneration.h                                    :+:      :+:    :+:   */
+/*   MapGeneration.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lberezyn <lberezyn@student.unit.ua>        +#+  +:+       +#+        */
+/*   By: liudmila <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 13:53:30 by lberezyn          #+#    #+#             */
-/*   Updated: 2019/02/09 13:53:30 by lberezyn         ###   ########.fr       */
+/*   Updated: 2019/02/18 05:11:52 by liudmila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BOMBERMAN_MAPGENERATION_H
 #define BOMBERMAN_MAPGENERATION_H
 
+#include <LogicCore/SquareInstance.h>
+#include <vector>
 
+class MapGenerator
+{
+public:
+	MapGenerator(int hardness);
+	MapGenerator(int width, int height, int hardness);
+	~MapGenerator();
+	std::vector<int> GenerateNewMap();
+	int GetWidth() const;
+	int GetHeight() const;
+
+private:
+	int mHardness = 0;
+	int mWidth = 30;
+	int mHeight = 20;
+};
 
 
 #endif //BOMBERMAN_MAPGENERATION_H
--- src/LogicCore/MapGeneration.cpp
+++ src/LogicCore/MapGeneration.cpp
@@ -1,13 +1,69 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MapGeneration.cpp                                    :+:      :+:    :+:   */
+/*   MapGeneration.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lberezyn <lberezyn@student.unit.ua>        +#+  +:+       +#+        */
+/*   By: liudmila <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 13:53:31 by lberezyn          #+#    #+#             */
-/*   Updated: 2019/02/09 13:53:31 by lberezyn         ###   ########.fr       */
+/*   Updated: 2019/02/18 05:15:33 by liudmila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <LogicCore/MapGeneration.hpp>
+
 #include "LogicCore/MapGeneration.hpp"
+
+
+MapGenerator::MapGenerator(int hardness) :
+	mHardness(hardness)
+{
+
+}
+
+MapGenerator::MapGenerator(int width, int height, int hardness) :
+	mWidth(width),
+	mHeight(height),
+	mHardness(hardness)
+{
+
+}
+
+MapGenerator::~MapGenerator() {
+
+}
+
+std::vector<int> MapGenerator::GenerateNewMap()
+{
+	std::vector<int> map;
+	int size = mWidth * mHeight;
+	map.reserve(size);
+
+	for (int i = 0; i < mHeight; ++i)
+	{
+		for (int j = 0; j < mWidth; ++j)
+		{
+			if (j == 0 || j == mWidth - 1 || i == 0 || i == mHeight - 1)
+				map.push_back((int)SquareType::Wall);
+			else if (i % 2 != 0 && j % 2 == 0)
+			{
+				map.push_back((int)SquareType::EmptySquare);
+			}
+			else
+			{
+				map.push_back((int)SquareType::Wall);
+			}
+
+		}
+
+	}
+	return map;
+}
+
+int MapGenerator::GetWidth() const {
+	return mWidth;
+}
+
+int MapGenerator::GetHeight() const {
+	return mHeight;
+}
