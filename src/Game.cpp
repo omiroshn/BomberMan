624d277a970c81f85c6593d87b22079b35807db2
arxcel <vadim.k.394010@gmail.com>
2019-02-24 23:20:22 +0200

    [GC] Added rendering from mapForRender

--- src/Game.cpp
+++ src/Game.cpp
@@ -7,8 +7,6 @@ Game::Game() : mTimeNow(SDL_GetPerformanceCounter()), mIsRunning(true)
     mRenderer = std::make_unique<Renderer>();
     mIManager = std::make_unique<InputManager>();
     loadResources();
-
-    mRenderer->initTestData();
 }
 
 Game::~Game()
@@ -25,14 +23,14 @@ void Game::start()
         mWindow->tickGui();
         if (mapLoader.MapIsLoaded())
         {
-            mRenderer->draw();
+            mRenderer->draw(mMap);
             mWindow->update();
             doAction(mIManager->processEvents(mWindow->getEvent()));
             mapLoader.UpdateMap();
         }
         else
         {
-            mMap = std::make_unique<MapForRendering>(mapLoader.GetMap(-1));
+            mMap = std::make_shared<MapForRendering>(mapLoader.GetMap(-1));
             mMap.get()->ParseMapBySquareInstances();
             mWindow->update();
         }
@@ -100,9 +98,9 @@ void Game::loadResources()
             basicSuite = glm::scale(basicSuite, glm::vec3(1.0f / scale_factorY, 1.0f / scale_factorY, 1.0f / scale_factorY));
             basicSuite = glm::translate(basicSuite, glm::vec3(-a->getAABB().getCenter().x, -a->getAABB().getCenter().y, -a->getAABB().getCenter().z));
             a->transform(basicSuite);
-            std::cout << a->getAABB().getMin().x << " -- " << a->getAABB().getMin().y << " -- " << a->getAABB().getMin().z << std::endl;
-            std::cout << a->getAABB().getMax().x << " -- " << a->getAABB().getMax().y << " -- " << a->getAABB().getMax().z << std::endl;
-            std::cout << a->getAABB().getCenter().x << " -- " << a->getAABB().getCenter().y << " -- " << a->getAABB().getCenter().z << std::endl << std::endl;
+//            std::cout << a->getAABB().getMin().x << " -- " << a->getAABB().getMin().y << " -- " << a->getAABB().getMin().z << std::endl;
+//            std::cout << a->getAABB().getMax().x << " -- " << a->getAABB().getMax().y << " -- " << a->getAABB().getMax().z << std::endl;
+//            std::cout << a->getAABB().getCenter().x << " -- " << a->getAABB().getCenter().y << " -- " << a->getAABB().getCenter().z << std::endl << std::endl;
         }
 
 
@@ -116,11 +114,32 @@ void Game::loadResources()
             basicCube = glm::translate(basicCube, glm::vec3(-b->getAABB().getCenter().x, -b->getAABB().getCenter().y, -b->getAABB().getCenter().z));
             basicCube = glm::rotate(basicCube, glm::radians(-90.0f), glm::vec3(0,0,1));
             b->transform(basicCube);
-            std::cout << b->getAABB().getMin().x << " -- " << b->getAABB().getMin().y << " -- " << b->getAABB().getMin().z << std::endl;
-            std::cout << b->getAABB().getMax().x << " -- " << b->getAABB().getMax().y << " -- " << b->getAABB().getMax().z << std::endl;
-            std::cout << b->getAABB().getCenter().x << " -- " << b->getAABB().getCenter().y << " -- " << b->getAABB().getCenter().z << std::endl;
+//            std::cout << b->getAABB().getMin().x << " -- " << b->getAABB().getMin().y << " -- " << b->getAABB().getMin().z << std::endl;
+//            std::cout << b->getAABB().getMax().x << " -- " << b->getAABB().getMax().y << " -- " << b->getAABB().getMax().z << std::endl;
+//            std::cout << b->getAABB().getCenter().x << " -- " << b->getAABB().getCenter().y << " -- " << b->getAABB().getCenter().z << std::endl;
         }
+        auto c = RESOURCES.loadModel("ground/ground.obj", "ground");
+        {
 
+            glm::mat4 basicCube = glm::mat4(1.0f);
+            auto scale_factorY = 2.0f;
+            basicCube = glm::scale(basicCube,
+                                   glm::vec3(1.0f / scale_factorY, 1.0f / scale_factorY, 1.0f / scale_factorY));
+            basicCube = glm::translate(basicCube, glm::vec3(-c->getAABB().getCenter().x, -c->getAABB().getCenter().y,
+                                                            -c->getAABB().getCenter().z));
+            c->transform(basicCube);
+        }
+        auto d = RESOURCES.loadModel("wall/wall.obj", "wall");
+        {
+
+            glm::mat4 basicCube = glm::mat4(1.0f);
+            auto scale_factorY = 2.0f;
+            basicCube = glm::scale(basicCube,
+                                   glm::vec3(1.0f / scale_factorY, 1.0f / scale_factorY, 1.0f / scale_factorY));
+            basicCube = glm::translate(basicCube, glm::vec3(-c->getAABB().getCenter().x, -c->getAABB().getCenter().y,
+                                                            -c->getAABB().getCenter().z));
+            d->transform(basicCube);
+        }
     }
     catch (CustomException &what)
     {
