190fe5e43b241cb6ff90a1ccccb54fc8e9fec3c2
Semyon Kamoza <s.kamoza@dragonlk.com>
2019-03-02 15:17:21 +0200

    Visual Studio fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 
 int main(int ac, char **av)
 {
-#if _WIN32_
-    RESOURCES.setBinFolder("");
+#if _WIN32 || _WIN32_
+    RESOURCES.setBinFolder("..\\");
 #else
     std::string exePath(av[0]);
     std::string exeFolder(exePath.substr(0, exePath.find_last_of("\\/") + 1));
