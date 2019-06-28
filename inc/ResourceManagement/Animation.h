73a608bc483bba8417cec36a2939fee7168ef0e0
vkozlov <vadim.k.394010@gmail.com>
2019-06-28 14:13:47 +0300

    add animation for main character

---
+++ inc/ResourceManagement/Animation.h
@@ -0,0 +1,35 @@
+//
+// Created by Vadym KOZLOV on 2019-06-28.
+//
+
+#ifndef BOMBERMAN_ANIMATION_H
+#define BOMBERMAN_ANIMATION_H
+#include <string>
+
+enum class AnimationType
+{
+    Idle,
+    Running,
+    Dying
+};
+
+class Animation
+{
+public:
+    explicit Animation(AnimationType = AnimationType::Idle);
+    Animation(double time, AnimationType = AnimationType::Idle);
+    ~Animation() = default;
+
+    double          getTime() const;
+    void            setTime(double time);
+    AnimationType   getType() const;
+    void            setType(AnimationType mType);
+    std::string     getName() const;
+    void            tick();
+private:
+    double mTime;
+    AnimationType mType;
+};
+
+
+#endif //BOMBERMAN_ANIMATION_H
