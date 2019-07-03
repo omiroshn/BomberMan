9520d9ce79cb1884b06c38bb019002eb069c556c
omiroshn <omiroshn@student.unit.ua>
2019-07-03 17:27:22 +0300

    changed enum and added explotion on buttons

--- src/InputManagement/InputManager.cpp
+++ src/InputManagement/InputManager.cpp
@@ -52,45 +52,7 @@ Action InputManager::processJoystickMotion(SDL_JoyAxisEvent jaxis)
 {
     if (jaxis.which == 0)
     {
-        if (jaxis.axis == 0)
-        {
-            
-            if (jaxis.value < -JOYSTICK_DEAD_ZONE)
-                xDir = -1;
-            else if (jaxis.value > JOYSTICK_DEAD_ZONE)
-                xDir = 1;
-            else
-                xDir = 0;
-        }
-        else if (jaxis.axis == 1)
-        {
-            if (jaxis.value < -JOYSTICK_DEAD_ZONE)
-                yDir = -1;
-            else if (jaxis.value > JOYSTICK_DEAD_ZONE)
-                yDir = 1;
-            else
-                yDir = 0;
-        }
-        double joystickAngle = atan2((double)yDir, (double)xDir) * (180.0 / M_PI);
-        
-        if (xDir == 0 && yDir == 0)
-            joystickAngle = 0;
-        if (joystickAngle == 0 && xDir == 1)
-            return Action::Right;
-        else if (joystickAngle == 45)
-            return Action::DownRight;
-        else if (joystickAngle == 90)
-            return Action::Backward;
-        else if (joystickAngle == 135)
-            return Action::DownLeft;
-        else if (joystickAngle == 180)
-            return Action::Left;
-        else if (joystickAngle == -135)
-            return Action::UpLeft;
-        else if (joystickAngle == -90)
-            return Action::Forward;
-        else if (joystickAngle == -45)
-            return Action::UpRight;
+        return Action::Joystick;
     }
     return Action::Nothing;
 }
@@ -100,9 +62,9 @@ Action InputManager::processJoystickButtonDown(SDL_JoyButtonEvent jbutton)
     if (jbutton.which == 0)
     {
         if (jbutton.button == 2)
-            return Action::Down;
+            return Action::JoystickButtonX;
         else if (jbutton.button == 1)
-            return Action::Up;
+            return Action::JoystickButtonY;
     }
     return Action::Nothing;
 }
