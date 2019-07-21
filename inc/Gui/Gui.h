22e10960c7bcd08e31a4eb61edf4a7fd80a2eeb9
Semen KAMOZA <skamoza@e1r9p2.unit.ua>
2019-07-21 21:52:16 +0300

    init

--- inc/Gui/Gui.h
+++ inc/Gui/Gui.h
@@ -3,8 +3,10 @@
 
 #include <imgui.h>
 #include <vector>
+#include <SDL2/SDL.h>
 
 #define  STANDARD_MENU_BUTTON {220, 48}
+
 
 enum class CurrentMenu
 {
@@ -31,11 +33,26 @@ public:
 	void GamePaused(bool state);
 	void SetBackground(const char* texture);
 
+	bool ImGui_Init(SDL_Window* window);
+	void ImGui_Shutdown();
+	void ImGui_NewFrame(SDL_Window* window);
+	void ImGui_RenderDrawData(ImDrawData* draw_data);
+	bool ImGui_ProcessEvent(SDL_Event* event);
+
+	void ImGui_InvalidateDeviceObjects();
+	bool ImGui_CreateDeviceObjects();
+
+
 private:
 	void ShowStartNewGameMenu();
 	void ShowLoadSavedGamesMenu();
 	void ShowSettingsMenu();
 	void ChangeStage(int next_stage);
+	void 		ImGui_CreateFontsTexture();
+
+	static const char* ImGui_GetClipboardText(void*);
+	static void 		ImGui_SetClipboardText(void*, const char *text);
+
 	bool mGameStarted = false;
 	bool mGamePaused = false; //make static in Game
 	ImGuiWindowFlags mWindow_flags = 0;
@@ -44,6 +61,22 @@ private:
 	float mWidth = 800.0f;
 	float mHeight = 600.0f;
 	std::vector<ImTextureID> mButtonsTextures;
+
+
+// SDL data
+Uint64       g_Time = 0;
+bool         g_MousePressed[3] = { false, false, false };
+SDL_Cursor*  g_MouseCursors[ImGuiMouseCursor_COUNT] = { 0 };
+
+static char*        g_ClipboardTextData;
+
+// OpenGL data
+char         g_GlslVersion[32]{"#version 330"};
+GLuint       g_FontTexture = 0;
+int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
+int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
+int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
+unsigned int g_VboHandle = 0,g_ElementsHandle = 0;
 };
 
 #endif //BOMBERMAN_GUI_H
